8c1def5acd335084189092c935535e3b6b7117b6
selengr <reza1997karbakhsh@gmail.com>
2023-05-21 14:13:00 +0330

    change the folders of the project

---
+++ app/(firstView)/blog/[id]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Introduction from '../../../../components/blog/introduction';
+import styles from "../../../../styles/components/blog/blog.module.css";
+import { useParams,useSearchParams } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
+import MainIdea from '../../../../components/blog/mainIdea';
+import Conclusion from "../../../../components/blog/conclusion";
+
+const Post = (props) => {
+     console.log("ppppp",props
+     );
+     let {searchParams, params :{id}} = props
+     
+ const param = useParams()
+ // const userouter = useRouter()
+ // const {values,getAll,keys} = useSearchParams()
+ // const {concat} = usePathname()
+ // console.log('router :>> ', param);
+
+
+
+  return (
+    <>
+         
+         <div className={styles["blog-page-master"]}>
+
+                <Introduction param={param}/>
+           <div className={styles["landing-article"]}>
+                <MainIdea />
+                 <Conclusion />
+           </div>
+
+         </div>
+    </>
+  )
+}
+
+export default Post
